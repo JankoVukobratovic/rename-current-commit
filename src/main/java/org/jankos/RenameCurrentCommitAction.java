07098269e1774537b39814aaf64f41fe570f2dab
Janko Vukobratovic <janko.vukobratovic05@gmail.com>
2025-04-06 02:39:02 +0200

    Initial version. "Works on my machine" I mean, it works, I tested it with ./gradlew runIde Nothing else to say, time to add tests?

--- src/main/java/org/jankos/RenameCurrentCommitAction.java
+++ src/main/java/org/jankos/RenameCurrentCommitAction.java
@@ -13,7 +13,6 @@ import git4idea.GitUtil;
 import git4idea.commands.Git;
 import org.jetbrains.annotations.NotNull;
 
-
 import java.util.List;
 
 public class RenameCurrentCommitAction extends AnAction{
