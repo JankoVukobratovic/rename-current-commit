696dcde10aecbce5b2a61b4b7c9b61698e289c80
Janko Vukobratovic <mc.xavpig@gmail.com>
2025-04-06 02:31:09 +0200

    Initial version. "Works on my machine" I mean, it works, I tested it with ./gradlew runIde Nothing else to say, time to add tests?

---
+++ src/main/java/org/jankos/RenameCurrentCommitAction.java
@@ -0,0 +1,56 @@
+package org.jankos;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import git4idea.commands.GitCommand;
+import git4idea.commands.GitLineHandler;
+import git4idea.repo.GitRepository;
+import git4idea.GitUtil;
+import git4idea.commands.Git;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class RenameCurrentCommitAction extends AnAction{
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getProject();
+        if (project == null) {
+            messageError("No project. Please open a project first.");
+            return;
+        }
+
+        List<GitRepository> repos = GitUtil.getRepositoryManager(project).getRepositories();
+        if (repos.isEmpty()) {
+            messageError("No Git repository found. Please open a Git repository.");
+            return;
+        }
+
+        GitRepository repo = repos.get(0);
+        String newMessage = Messages.showInputDialog(project, "New commit message:", "Rename Commit", null);
+        if (newMessage == null || newMessage.trim().isEmpty()) {
+            messageError("Empty message. Don't do that.");
+            return;
+        }
+
+        new Task.Backgroundable(project, "Amending git commit") {
+            @Override
+            public void run(@NotNull ProgressIndicator indicator) {
+                GitLineHandler handler = new GitLineHandler(project, repo.getRoot(), GitCommand.COMMIT);
+                handler.addParameters("--amend", "-m", newMessage);
+                Git.getInstance().runCommand(handler);
+
+                // Mark repo as dirty to update UI correctly
+                repo.update();
+            }
+        }.queue();
+    }
+
+    private void messageError(String message) {
+        Messages.showErrorDialog(message, "Rename Commit - Error");
+    }
+}
